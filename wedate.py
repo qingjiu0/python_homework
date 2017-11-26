9e5e789c0745de4205885e5e50a86bb5f74ebd12
清玖 <qingjiu@qingjiudeMacBook-Air.local>
2017-11-26 20:46:59 +0800

    微侠web登陆页

---
+++ wedate.py
@@ -0,0 +1,10 @@
+import os
+from selenium import webdriver
+driver = webdriver.Chrome (executable_path="./chromedriver")
+driver.get("http://www.wedate.me/")
+login = driver.find_element_by_css_selector("#home > div > div > div > div > div > div > div.hidden-xs.hidden-sm > div:nth-child(2) > a")
+login.click()
+phonelogin = driver.find_element_by_css_selector("#logintab > li:nth-child(2) > a")
+phonelogin.click()
+text = driver.find_element_by_css_selector("#mobile")
+textlete.click()
