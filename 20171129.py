ae09d38f6ae92d923f7b1c6baf95676ff2c0566e
清玖 <qingjiu@qingjiudeMacBook-Air.local>
2017-11-30 22:00:09 +0800

    20171129作业-python字符串的操作

---
+++ 20171129.py
@@ -0,0 +1,10 @@
+name = "小明"
+address = "北京"
+age = 16
+money = 100.001
+
+print(name,address)
+print(name+address)
+print(name,  address)
+print(name+"    "+address)
+print("my name is %s,i am live in %s,i am %d year old,i have %a money"%(name,address,age,money))
