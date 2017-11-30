66e3a3e3721ac7f7d86320315bd50ee646b2243a
清玖 <qingjiu@qingjiudeMacBook-Air.local>
2017-11-30 16:57:20 +0800

    更新

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,21 @@
-print("helloworld")
+
+
+a=10
+b=20
+if a:
+    print b
+else:
+    print a
+if (a):
+    print("b")
+else:
+    print("a")
+
+if (a and b):
+    print("line1",b)
+else:
+    print("line1",a)
+if (a and b):
+    print("line1-b")
+else:
+    print("line1-a")
